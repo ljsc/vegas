bcdfd2d4d1c895d681814bd5f1736bb1f11ae796
Lou Scoras <louis.j.scoras@gmail.com>
2011-06-30 21:22:00 -0400

    Started implementing image parser

---
+++ lib/vegasfs/parsers/image.rb
@@ -0,0 +1,38 @@
+require 'net/http'
+
+module VegasFS::Parsers
+  class Image
+    attr_reader :url
+
+    SERVICE_HOST = 'yfrog.com'
+
+    def initialize(tweet)
+      if m = tweet.match( %r|(?:http://)?#{SERVICE_HOST}/\w+| )
+        @url = m.to_s
+      else
+        @url = nil
+      end
+    end
+
+    def contains_images?
+      !@url.nil?
+    end
+
+    def image_data
+      response = nil
+
+      Net::HTTP.start(SERVICE_HOST) do |http|
+        response = http.get(URI.parse(@url).path + ':medium')
+
+        if response.code.to_s =~ /\A3\d\d/
+          new_location = URI.parse(response['Location'])
+          Net::HTTP.start(new_location.host) do |http|
+            response = http.get(new_location.path + '?' + new_location.query)
+          end
+        end
+      end
+
+      response.body
+    end
+  end
+end
