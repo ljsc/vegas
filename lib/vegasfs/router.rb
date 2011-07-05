2c60cf564d9d3a15a7a101c9335c1650c42fdf26
Catalino Cuadrado <catalino@gwu.edu>
2011-07-04 21:37:01 -0400

    Moved helpers into own module for testing purposes
    Moved the twitter helper methods into VegasFS::TwitterHelpers module.

--- lib/vegasfs/router.rb
+++ lib/vegasfs/router.rb
@@ -1,4 +1,46 @@
+module VegasFS::TwitterHelpers
+
+  def get_user_info(username)
+    url = 'http://api.twitter.com/1/users/show.json?screen_name='+ username
+    response = Net::HTTP.get(URI.parse(url))
+    info = JSON.parse(response)
+  end
+
+  def get_user_mentions
+    client = configure_authentication
+    mentions = client.mentions
+    client.end_session
+    mentions.to_json
+  end
+
+  def get_tweet_by_id(id)
+    tweet = Twitter.status(id)
+    {:message => tweet.text, :author => tweet.user.screen_name, :date => tweet.created_at} 
+  end
+  def configure_authentication
+    consumer_secret = "18yElxR1Zf8ESVkl3k7XQZxyAPWngz5iM69nbhH7yE"
+    consumer_key = "zQ727fZBHDIv36pKhr2Hg"
+
+    Twitter.configure do |config|
+      config.consumer_key = consumer_key
+      config.consumer_secret = consumer_secret
+      config.oauth_token = "157879876-iSPfgtHxw8QSAj6cJl0uYTbDTV1kfxsw8Tgi1QGK"
+      config.oauth_token_secret = "XiI1kkuGgvqZNc4mGIGkPxjcr19p9PVxhT7m0M"
+      Twitter::Client.new
+    end
+  end
+
+  def get_latest_tweets(num_of_tweets)
+    client = configure_authentication
+    latest = client.home_timeline({:count => num_of_tweets})
+    client.end_session
+    info = latest.to_json
+  end 
+
+end
+
 class VegasFS::Router < Sinatra::Base
+ include VegasFS::TwitterHelpers
   configure :test do
     set :show_exceptions, false
     set :raise_errors, true
@@ -89,47 +131,6 @@ class VegasFS::Router < Sinatra::Base
   get '/tweet/latest.txt' do 
       [200, {'Content-Type' => 'text/html'} , get_latest_tweets(20)]
   end 
-
-
-# Twitter Helper Methods
-  def get_user_info(username)
-    url = 'http://api.twitter.com/1/users/show.json?screen_name='+ username
-    response = Net::HTTP.get(URI.parse(url))
-    info = JSON.parse(response)
-  end
-
-  def get_user_mentions
-    client = configure_authentication
-    mentions = client.mentions
-    client.end_session
-    mentions.to_json
-  end
-
-  def get_tweet_by_id(id)
-    tweet = Twitter.status(id)
-    {:message => tweet.text, :author => tweet.user.screen_name, :date => tweet.created_at} 
-  end
-  def configure_authentication
-    consumer_secret = "18yElxR1Zf8ESVkl3k7XQZxyAPWngz5iM69nbhH7yE"
-    consumer_key = "zQ727fZBHDIv36pKhr2Hg"
-
-    Twitter.configure do |config|
-      config.consumer_key = consumer_key
-      config.consumer_secret = consumer_secret
-      config.oauth_token = "157879876-iSPfgtHxw8QSAj6cJl0uYTbDTV1kfxsw8Tgi1QGK"
-      config.oauth_token_secret = "XiI1kkuGgvqZNc4mGIGkPxjcr19p9PVxhT7m0M"
-      Twitter::Client.new
-    end
-  end
-
-  def get_latest_tweets(num_of_tweets)
-    client = configure_authentication
-    latest = client.home_timeline({:count => num_of_tweets})
-    client.end_session
-    info = latest.to_json
-  end 
-
-
 
 end
 
